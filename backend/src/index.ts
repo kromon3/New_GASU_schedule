4cdf173567d7d6011e88852616a961162955b0a9
kiril <kirillpley56@gmail.com>
2026-04-16 11:46:02 +0300

    feat:фиксы

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -30,7 +30,7 @@ const readLessons = async ():Promise<Lesson[]> => {
     try {
         const content = await readFile(DB_PATH, 'utf-8');
         return JSON.parse(content);
-    } catch (err) {
+    } catch (err: any) {
         console.error(' Ошибка чтения:', err.message);
         return [];
     }
@@ -40,7 +40,7 @@ app.get('/lessons', async (req: Request, res: Response) => {
     try {
         const lessons = await readLessons();
         res.status(200).json(lessons);
-    } catch (err) {
+    } catch (err: any) {
         res.status(500).json({ message: err.message });
     }
 });
@@ -54,7 +54,7 @@ app.get('/lessons/:id', async (req: Request, res: Response) => {
             return res.status(404).json({ message: `Урок с ID ${id} не найден` });
         }
         res.status(200).json(findLesson);
-    }catch (err) {
+    }catch (err: any) {
         res.status(500).json({ message: err.message });
     }
 })
@@ -63,7 +63,7 @@ app.post('/lessons', async (req: Request, res: Response) => {
     try{
         const lesson = await readLessons();
         const newLesson = {
-            id: Date.now(),
+            id: Date.now().toString(),
             subject: req.body.subject,
             time:{
                 start: req.body.time.start,
@@ -80,7 +80,7 @@ app.post('/lessons', async (req: Request, res: Response) => {
         await writeFile(DB_PATH, JSON.stringify(lesson, null, 2), 'utf-8');
         res.status(201).json(newLesson);
     }
-    catch (err) {
+    catch (err: any) {
         res.status(500).json({ message: err.message });
     }
 })
@@ -109,7 +109,7 @@ app.patch('/lessons/:id', async (req: Request, res: Response) => {
             return res.status(200).json(updatedLesson);
         }
     }
-    catch (err) {
+    catch (err: any) {
         res.status(500).json({ message: err.message });
     }
 })
@@ -127,7 +127,7 @@ app.delete('/lessons/:id', async (req: Request, res: Response) => {
         await writeFile(DB_PATH, JSON.stringify(updateLessons, null, 2), 'utf-8');
         return res.status(204).send();
     }
-    catch (err) {
+    catch (err: any) {
         res.status(500).json({ message: err.message });
     }
 
